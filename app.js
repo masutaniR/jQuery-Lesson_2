cc4eb39f39cad2fb15d4c976286b5a7976dd12a7
masutaniR <popohu87@gmail.com>
2021-01-17 16:27:06 +0900

    [Start] JS-6

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 $(function (){
-  $('.box1').slideDown(3000, function (){
-    $('.box1').css({
-      'background-color': '#0000ff',
-      'height': '100px'
-    }).slideUp(3000);
+  $('.box1').mouseover(function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
   });
 });
